e92983f181aae60d76b75077b4369d3018b05175
Janardhan Pulivarthi <j143@protonmail.com>
2023-07-16 10:24:20 +0530

    try to print syn, ack packts with seq numbers

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import signal
 import socket
 import sys
+import platform
+import ifaddr
+import subprocess
+from scapy.all import *
 
 def signal_handler(sig, frame):
     print('Shutting down server...')
@@ -23,6 +27,25 @@ server_socket.bind(server_address)
 server_socket.listen(1)
 print(f"Server listening on {server_address[0]}:{server_address[1]}")
 
+# Get the list of network interfaces based on the platform
+# if platform.system() == 'Linux':
+#     # Linux command to get the list of interfaces
+#     interfaces_command = 'ip link show | awk \'{print $2}\' | sed \'s/://\''
+# else:
+# Windows command to get the list of interfaces
+interfaces_command = 'wmic nic get NetConnectionID'
+
+# Execute the command to get the interfaces
+interfaces_process = subprocess.Popen(interfaces_command, shell=True, stdout=subprocess.PIPE)
+interfaces_output = interfaces_process.stdout.read().decode().splitlines()
+
+# Filter out loopback interfaces if needed
+interfaces = [
+    interface_name
+    for interface_name in interfaces_output
+    if 'loopback' not in interface_name.lower()
+]
+
 while True:
 
     # Check for the SIGINT signal
@@ -32,6 +55,18 @@ while True:
     print("Waiting for a connection...")
     client_socket, client_address = server_socket.accept()
     print(f"Connection from {client_address[0]}:{client_address[1]}")
+
+    # Start capturing packets
+    packets = sniff(filter="tcp", iface=['Ethernet','vEthernet (Default Switch)'], count=10)
+
+    # Print the captured packets
+    for packet in packets:
+        if TCP in packet:
+            print(f"Sequence Number: {packet[TCP].seq}")
+            if packet[TCP].flags.S:
+                print("SYN packet")
+            if packet[TCP].flags.A:
+                print("ACK packet")
 
     # Receive data from the client
     data = client_socket.recv(1024)
